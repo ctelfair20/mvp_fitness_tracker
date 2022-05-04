8efbb25a751ed5bf484162602a32281d212ecc5f
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-05-04 11:46:46 -0500

    adds mongoose connection

--- database/index.js
+++ database/index.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/fitness').
-  catch(error => {
+mongoose.connect('mongodb://localhost:27017/fitness')
+  .catch(error => {
     console.log('whoops...never connected')
   });
 
 mongoose.connection.on('error', err => {
   console.log('huh? Error after conection');
 });
+
+const workoutSchema = new mongoose.Schema({
+  exercise: String,
+  sets: Number,
+  repititions: Number,
+  date: Date
+});
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout
