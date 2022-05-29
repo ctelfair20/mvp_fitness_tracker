25b1472668475d25a65f4718bf0f09bb453ec7cd
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-05-29 14:53:12 -0500

    form submits successfully

--- src/components/MantineE.jsx
+++ src/components/MantineE.jsx
@@ -12,30 +12,30 @@ function MantineE() {
     },
     validate: {
       date: (value) => (
-        /(0[1-9]|1[012])[- \/.](0[1-9]|[12][0-9]|3[01])[- \/.](19|20)\d\d/.test(value) ? null : 'date-error'
+        /(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d/.test(value) ? null : 'Please use mm/dd/yyyy format'
       ),
     },
-  })
+  });
 
-  function handleSubmit(values) {
-    console.log(values);
-    console.log('form val :', typeof form.values.date)
-  }
+  // function handleSubmit(values) {
+  //   console.log(values);
+  //   console.log('form val :', typeof form.values.date)
+  // }
 
   return (
     <Box sx={{ maxWidth: 300 }}>
-      <form onSubmit={form.onSubmit(handleSubmit)}>
+      <form onSubmit={form.onSubmit((values) => console.log(values))}>
         <TextInput
           required
           label="Date"
           placeholder="05/16/2022"
-          onChange={(event) => form.setFieldValue('date', event.currentTarget.value)}
+          {...form.getInputProps('date')}
         />
         <TextInput
           required
           label="Exercise"
           placeholder="Sit-ups"
-          onChange={(event) => form.setFieldValue('exercise', event.currentTarget.value)}
+          {...form.getInputProps('exercise')}
         />
         <NumberInput
           label="Sets"
