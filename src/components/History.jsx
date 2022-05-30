b533dbdae80e5fee242d78e57f5d7549cff85749
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-05-30 16:35:45 -0500

    styles the users history

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { Button, Group, Box } from '@mantine/core';
 
 function History({ workouts }) {
 
   const historyMapper = function (arr) {
     return arr.map((obj) => {
       return (
-        <div key={obj._id}>
-          <>
-            <h3>Exercises: {obj.exercise}</h3>
-            <h3>Sets: {obj.sets}</h3>
-            <h3>Repititions: {obj.repititions}</h3>
-          </>
-        </div>
+        <Box
+          key={obj._id}
+          sx={(theme) => ({
+            color: theme.colors.blue[4],
+            borderTop: 'black 2px solid',
+            margin: '15px',
+            padding: '5px',
+            maxWidth: 300,
+
+            '&:hover': {
+              color: theme.colors.blue[8],
+            },
+          })}>
+
+          <h3>Exercises: {obj.exercise}</h3>
+          <h3>Sets: {obj.sets}</h3>
+          <h3>Repititions: {obj.repititions}</h3>
+          <Group position="right" mt="md">
+            <Button>Edit!</Button>
+          </Group>
+        </Box>
       )
     })
   }
