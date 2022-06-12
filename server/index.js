ae8f4c5ad9d90e6e72e1a24606557500bfb85d1b
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-06-12 12:04:57 -0500

    reorganizes server file

--- server/index.js
+++ server/index.js
@@ -1,25 +1,19 @@
-const path = require('path');
 const express = require('express');
 const Workout = require('../database/index');
+const cors = require('cors');
 
 const app = express();
-var cors = require('cors');
 const port = 4000;
-// const publicPath = path.join(__dirname, '..', 'public');
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 app.get('/workout', (req, res) => {
   console.log('req', req.query)
   Workout.find(req.query)
     .then((data) => {
-      // console.log(data);
       res.json(data);
     }).catch(err => {
       res.send(err);
