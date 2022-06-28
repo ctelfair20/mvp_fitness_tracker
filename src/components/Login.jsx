b108580bd7e01c8c5fb0ca34b4147d8b71ff64bd
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-06-28 14:22:39 -0500

    creates shell of a form for login box

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 import { TextInput, Button, Group, Box } from '@mantine/core';
 
 function Login() {
-  return (
-
-  )
+  <Box className="login-box" sx={{ maxWidth: 300 }}>
+    <form>
+      <TextInput
+        label="Email"
+        placeholder="abc@123.com" />
+      <TextInput
+        label="Password"
+      />
+      <Group position="center" mt="md">
+        <Button type="submit">Submit</Button>
+      </Group>
+    </form>
+  </Box>
 }
 
 export default Login;
