06c8dacc2e27c8fc36cfa52d5182c09c2e990fbd
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-06-28 13:31:05 -0500

    adds highlight to tabs in the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,10 +4,8 @@ import './NavBar.css';
 function NavBar() {
   return (
     <div className="nav-bar">
-      <div className="tab-box">
-        <a href="./home" className="tab">Home</a>
-        <a href="./login" className="tab">Login</a>
-      </div>
+      <a href="./home" className="tab">Home</a>
+      <a href="./login" className="tab">Login</a>
     </div>
   )
 }
