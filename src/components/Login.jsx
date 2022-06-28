e82169b504bb58cbe6e6ce081202ad4971d159d8
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-06-28 16:23:50 -0500

    adds style to login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import NavBar from './NavBar.jsx';
 import { TextInput, Button, Group, Box } from '@mantine/core';
+import './Login.css';
 
 function Login() {
   return (
     <div className="login-container">
       <NavBar />
-      <Box className="login-box" sx={{ maxWidth: 300 }}>
-        <form>
-          <TextInput
-            required
-            label="Email"
-            placeholder="abc@123.com" />
-          <TextInput
-            required
-            label="Password"
-          />
-          <Group position="center" mt="md">
-            <Button type="submit">Submit</Button>
-          </Group>
-        </form>
+      <h1>Stay fit with FitTrack!</h1>
+      <Box className="login-outer-box" sx={{ maxWidth: 400 }}>
+        <Box className="login-box" sx={{ maxWidth: 400 }}>
+          <form>
+            <TextInput
+              required
+              label="Email"
+              placeholder="abc@123.com" />
+            <TextInput
+              required
+              label="Password"
+            />
+            <Group position="right" mt="md">
+              <Button type="submit" variant="outline" color="dark">Submit</Button>
+            </Group>
+          </form>
+        </Box>
       </Box>
     </div>
   )
