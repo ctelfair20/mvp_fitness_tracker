6b1d7a9d60f0631d34bee5667d4403938e27199e
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-06-28 14:30:03 -0500

    makes login fields required for submission

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,9 +5,11 @@ function Login() {
   <Box className="login-box" sx={{ maxWidth: 300 }}>
     <form>
       <TextInput
+        required
         label="Email"
         placeholder="abc@123.com" />
       <TextInput
+        required
         label="Password"
       />
       <Group position="center" mt="md">
