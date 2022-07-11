1515639bb81b311a024b9e53a9539abbd8c9e847
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-07-10 23:46:04 -0500

    moved delete button to center of column and resized bottom half of page

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -30,7 +30,7 @@ function History({ workouts }) {
 
   if (workouts.length !== 0) {
     return (
-      <Table className="history-table" striped highlightOnHover>
+      <Table className="history-table" horizontalSpacing="sm" striped highlightOnHover>
         <thead>
           <tr>
             <th>Exercise</th>
@@ -38,7 +38,7 @@ function History({ workouts }) {
             <th>Repitition #</th>
             {/* notes need to be added to the databse schema */}
             <th>Notes</th>
-            <th>Made a mistake?</th>
+            {/* <th>Made a mistake?</th> */}
           </tr>
         </thead>
         <tbody>{rows}</tbody>
