7b28d908ecf32b5763f1d2d1f3006d2fe073d279
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-07-11 14:21:44 -0500

    creates handle change function for sort component

--- src/components/EmptySearch.js
+++ src/components/EmptySearch.js
@@ -1,9 +1,7 @@
 import { Box } from '@mantine/core';
 
 const EmptySearch = ({ searchedWorkouts, searchClick }) => {
-  console.log(searchClick, searchedWorkouts.length);
   if ((searchedWorkouts.length === 0) && searchClick) {
-    console.log('len', searchedWorkouts.length, 'clicked?', searchClick);
     return (
       <Box
         sx={(theme) => ({
--- src/components/SortBox.jsx
+++ src/components/SortBox.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { Text, Select } from '@mantine/core';
+import React, { useState, } from 'react';
+import { Select } from '@mantine/core';
 
 function SortBox({ workouts }) {
+
+  const [value, setValue] = useState('');
+
+  function handleChange() {
+    setValue();
+    console.log(value);
+  }
+
   if (workouts.length !== 0) {
     return (
       <Select
         className="sort-box-container"
+        value={value}
+        onChange={handleChange}
         clearable
         label="Sort by"
         placeholder="Pick one"
