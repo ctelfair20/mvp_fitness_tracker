0a0ace515b1c13737eb045c85d7709a66eb11099
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-07-14 15:21:04 -0500

    created a second test route to add history to the database

--- server/controllers/history.js
+++ server/controllers/history.js
@@ -1,5 +1,9 @@
 module.exports = {
   getHistory: (req, res) => {
-    res.send('THIS WORKS??');
-  }
+    res.send('Should get all history for a given day');
+  },
+
+  createHistory: (req, res) => {
+    res.send('Should create a new history for a given day');
+  },
 }
--- server/routes/history.js
+++ server/routes/history.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { getHistory } = require('../controllers/history');
+const { getHistory, createHistory } = require('../controllers/history');
 const router = express.Router();
 
 router.get('/', getHistory);
+router.get('/', createHistory);
 
 module.exports = router;
