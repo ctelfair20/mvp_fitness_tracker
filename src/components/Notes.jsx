7740cce39d3cb20955c45167dd5acca74c005247
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-08-17 22:38:54 -0500

    adds onBlur handler

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,15 +1,31 @@
 import React, { useState } from 'react';
 import { Textarea } from '@mantine/core';
+import axios from 'axios';
 
-const Notes = ({ id }) => {
+const Notes = ({ record }) => {
 
   const [text, setText] = useState('');
+
+  const handleOnBlur = async (id) => {
+    try {
+      const postResponse = await axios({
+        url: `http://localhost:4000/${id}`,
+        method: 'patch',
+        data: {
+          notes: text,
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   return (
     <td>
       <Textarea
+        onBlur={(e) => { handleOnBlur(record._id) }}
         className="notes-container"
-        value={text}
+        value={text ? text : ''}
         onChange={(e) => {
           setText(e.currentTarget.value);
         }}
